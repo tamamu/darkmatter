9aa2ff04afbc5b8c3e60d86a450e375b8023fd21
tamamu <tamamu.1r1s@gmail.com>
2017-09-18 02:26:42 +0900

    Disable auto-loading plugins

--- src/settings.lisp
+++ src/settings.lisp
@@ -7,13 +7,22 @@
 (in-package :cl-user)
 (defpackage darkmatter.settings
   (:use :cl)
-  (:export :*plugins*))
+  (:export :*plugins*
+           :load-settings))
 (in-package :darkmatter.settings)
 
 (defparameter *plugins*
   (list))
 
+(defparameter *settings-file-path*
+  (merge-pathnames ".darkmatter.conf" (user-homedir-pathname)))
+
+(defun load-settings ()
+  (if (probe-file *settings-file-path*)
+      (load *settings-file-path*)))
+#|
 (let ((path (merge-pathnames ".darkmatter.conf" (user-homedir-pathname))))
   (if (probe-file path)
       (load path)))
+|#
 
